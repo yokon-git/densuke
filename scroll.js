9be9a82914d0aa44c42dfa052bf5880c2f98916c
yokon-git <120079421+yokon-git@users.noreply.github.com>
2023-01-09 13:18:11 +0900

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,6 @@
+$('a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top-150;//idの上部の距離からHeaderの高さを引いた値を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
